de9f615781acd67a6455662c71ff50836d220bc1
Rubén Martínez <ruben.martinez1@gmail.com>
2017-03-23 23:29:41 +0100

    Added new property, required, to the Property attribute in configuration interface.

--- DotNet.MultiSourceConfiguration/PropertyAttribute.cs
+++ DotNet.MultiSourceConfiguration/PropertyAttribute.cs
@@ -6,15 +6,23 @@ namespace MultiSourceConfiguration.Config
     public class PropertyAttribute : Attribute
     {
         private readonly string property;
+		private bool required;
 
         public PropertyAttribute(string property)
         {
             this.property = property;
+			required = false;
         }
 
         public string Property
         {
             get { return property; }
         }
+
+		public bool Required { 
+			get { return required; }
+
+			set{ required = value; }
+		}
     }
 }
